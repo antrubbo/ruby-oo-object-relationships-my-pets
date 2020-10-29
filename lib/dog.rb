1266f9e63be75fb9880f2118cdac2f7dff096d03
antrubbo <anthony.rubbo@gmail.com>
2020-10-29 15:26:51 -0400

    Tests passing

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog
-  # code goes here
+  attr_reader :name
+  attr_accessor :mood, :owner
+
+  @@all = []
+
+  def initialize(name, owner, mood="nervous")
+    @name = name
+    @mood = mood
+    @owner = owner
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
